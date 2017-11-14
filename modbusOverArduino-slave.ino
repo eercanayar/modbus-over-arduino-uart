d791938519bb3ce59bac68ed5faf4e5d8b51ad8f
eercanayar <eercanayar@gmail.com>
2017-11-14 19:44:18 +0300

    other fc lines

--- modbusOverArduino-slave.ino
+++ modbusOverArduino-slave.ino
@@ -1,12 +1,14 @@
 const char slaveId1 = 48;
 const char slaveId2 = 49;
 String strRead;
+int potPin = A0;
+
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(13, OUTPUT);
-
+  
   pinMode(5, INPUT);
   pinMode(6, INPUT);
   pinMode(7, INPUT);
@@ -15,7 +17,7 @@ void setup()
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   
-  digitalWrite(13, LOW);    
+  digitalWrite(13, LOW);
 }
 
 void loop()
@@ -39,12 +41,51 @@ void loop()
           int calc = digitalRead(5) + digitalRead(6)*2 + digitalRead(7)*4;
           Serial.print(calc);
           
+          
+         // int lrc1 = calcCheckSum({58,slaveId1,slaveId2,48,49,48,49,48,calc,13,10},11);
+         // Serial.print(lrc1);
           Serial.write(13); // CR
           Serial.write(10); // LF
         }
         else if (strRead[3] == 48 && strRead[4] == 53) // we're in fc05
         {
+          Serial.write(58);
+          Serial.write(slaveId1);
+          Serial.write(slaveId2);
+          Serial.write(48);
+          Serial.write(53);
+          Serial.write(48); // byte count 01
+          Serial.write(49); // Sor
+          Serial.write(48);
+          Serial.write(7); 
           
+        //  int lrc5 = calcCheckSum({58,slaveId1,slaveId2,48,53,48,49,48,7,13,10},11);
+        //  Serial.write(lrc5);
+         Serial.write(13); // CR
+          Serial.write(10); // LF
+        }   else if (strRead[3] == 48 && strRead[4] == 52) // we're in fc04
+       
+        {
+          Serial.write(58);
+          Serial.write(slaveId1);
+          Serial.write(slaveId2);
+          Serial.write(48);
+          Serial.write(52);
+          Serial.write(48); // byte count 02
+          Serial.write(50);
+          Serial.write(48);
+          int deger = analogRead(potPin);
+          String degerInHex = String(deger, HEX);
+          char charBuf[10];
+          degerInHex.toCharArray(charBuf, 10);
+          sprintf(charBuf,"%02X", charBuf);
+          
+          Serial.print(degerInHex);
+
+          //int lrc2 = calcCheckSum({58,48,49,48,50,48,49,48,buf,13,10},11);
+         // Serial.print(lrc2);
+           Serial.write(13); // CR
+          Serial.write(10); // LF
         }
         delay(50);
       } else {
@@ -55,7 +96,14 @@ void loop()
      }
    
   }
-  
+
 
 }
 
+  int * calcCheckSum (int* pack, int packSize) {
+   int sum = 0;
+   for (int i=0; i < (packSize-1); i++) sum += pack[i];
+   sum = sum & 0xFF;
+   pack[packSize-1] = 0x100 - sum;
+   return pack;
+  }
