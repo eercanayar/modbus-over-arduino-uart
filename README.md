592192e6e2637d93ad5e02418bea2694585134e2
eercanayar <eercanayar@gmail.com>
2017-11-15 20:22:29 +0300

    detailed comments

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # modbus-over-arduino-uart
 a simple ascii modbus implementation on pure arduino uuart tx/rx
 
-to do:
+## to do:
 * LRC calculation
-* implementation of starting index and amount of registers to read
+* implementation of starting index and amount of registers to read (now ignores indexes, reads all)
+* error message packets for every exceptiion
--- modbusOverArduino-slave.ino
+++ modbusOverArduino-slave.ino
@@ -1,30 +1,28 @@
-
-const char slaveId1 = 48;
+const char slaveId1 = 48; // define the address here
 const char slaveId2 = 49;
+
 String strRead;
 int potPin = A0;
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
+  Serial.setTimeout(1000); // 1000ms timeout if there is data but no CLRF
   
-  pinMode(5, INPUT);
+  pinMode(5, INPUT); // prepare 2 buttons
   pinMode(6, INPUT);
-  pinMode(7, INPUT);
-
-  pinMode(8, OUTPUT);
+  
+  pinMode(8, OUTPUT); // prepare 3 LEDs
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   
-  digitalWrite(13, LOW);
 }
 
 void loop()
 {
   
   if (Serial.available()>0) {
-      strRead = Serial.readStringUntil('\r\n');
+      strRead = Serial.readStringUntil('\r\n'); // CL RF bit for modbus ascii
      
     if(strRead[0]==58) { // check start bit
       if(strRead[1]==slaveId1 && strRead[2]==slaveId2) { // check address
@@ -38,11 +36,11 @@ void loop()
           Serial.write(48); // byte count 01
           Serial.write(49);
           Serial.write(48);
-          int calc = digitalRead(5) + digitalRead(6)*2;
+          int calc = digitalRead(5) + digitalRead(6)*2; // basic binary math
           Serial.print(calc);
           
-          Serial.write(70); // LRC
-         Serial.write(48); // 
+          Serial.write(70); // todo: LRC
+          Serial.write(48);
          
           Serial.write(13); // CR
           Serial.write(10); // LF
@@ -51,9 +49,9 @@ void loop()
         {
           
           Serial.write(58);
-         
           Serial.write(slaveId1);
           Serial.write(slaveId2);
+          
           if(strRead[11]==70 && strRead[12]==70) {
             digitalWrite(strRead[8]-40, HIGH); // ascii to pin number trick here
           } else if(strRead[11]==48 && strRead[12]==48) { 
@@ -71,11 +69,11 @@ void loop()
           Serial.write(strRead[11]);
           Serial.write(strRead[12]);
 
-          Serial.write(70); // LRC
-         Serial.write(48); // 
+          Serial.write(70); //todo:  LRC
+          Serial.write(48);
   
-         Serial.write(13); // CR
-         Serial.write(10); // LF
+          Serial.write(13); // CR
+          Serial.write(10); // LF
         } else if (strRead[3] == 48 && strRead[4] == 52) // we're in fc04
          {
           Serial.write(58);
@@ -86,23 +84,24 @@ void loop()
           Serial.write(48); // byte count 02
           Serial.write(50);
           Serial.write(48);
-          int deger = analogRead(potPin);
-          String degerInHex = String(deger, HEX);
+          
+          int analogVal = analogRead(potPin);
+          String analogValInHex = String(analogVal, HEX);
           char charBuf[10];
-          degerInHex.toCharArray(charBuf, 10);
+          analogValInHex.toCharArray(charBuf, 10);
           sprintf(charBuf,"%02X", charBuf);
           
-          Serial.print(degerInHex);
+          Serial.print(analogValInHex);
 
-         Serial.write(70); // LRC
-         Serial.write(48); // 
+          Serial.write(70); // todo: LRC
+          Serial.write(48);
          
-           Serial.write(13); // CR
+          Serial.write(13); // CR
           Serial.write(10); // LF
         }
-        delay(50);
+        delay(50); 
       } else {
-        Serial.end();
+        Serial.end(); // for deterministic communication
         delay(50);
         Serial.begin(9600);
       }
